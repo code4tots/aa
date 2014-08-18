51cfd97d6f6d080ae4dc7f59482cea9c7fc0f511
math4tots <math4tots@gmail.com>
2014-08-17 19:42:46 -0700

    aa-questions medium finished

--- sql/questions/import_db.sql
+++ sql/questions/import_db.sql
@@ -8,18 +8,20 @@ CREATE TABLE users (
 
 CREATE TABLE questions (
 	question_id INT,
+	user_id     INT,
 	title       TEXT,
 	body        TEXT,
 	
-	PRIMARY KEY (question_id)
+	PRIMARY KEY (question_id),
+	FOREIGN KEY (user_id)     REFERENCES users(user_id)
 );
 
 CREATE TABLE question_followers (
-	follow_id   INT,
-	user_id     INT,
-	question_id INT,
+	question_follower_id   INT,
+	user_id                INT,
+	question_id            INT,
 	
-	PRIMARY KEY (follow_id),
+	PRIMARY KEY (question_follower_id),
 	FOREIGN KEY (user_id)     REFERENCES users(user_id),
 	FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
@@ -27,19 +29,21 @@ CREATE TABLE question_followers (
 
 CREATE TABLE replies (
 	reply_id    INT,
-	parent      INT,   /* parent reply, may be null */
+	parent_id   INT,   /* parent reply, may be null */
 	user_id     INT,   /* user that made this reply */
 	question_id INT,   /* question this is a reply to */
 	body        TEXT,  /* body of the reply */
 	PRIMARY KEY (reply_id),
-	FOREIGN KEY (parent)      REFERENCES replies(reply_id),
+	FOREIGN KEY (parent_id)   REFERENCES replies(reply_id),
 	FOREIGN KEY (user_id)     REFERENCES users(user_id),
 	FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
 
 CREATE TABLE question_likes (
+	question_like_id INT,
 	user_id     INT,
 	question_id INT,
+	PRIMARY KEY (question_like_id),
 	FOREIGN KEY (user_id)     REFERENCES users(user_id),
 	FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
@@ -52,22 +56,26 @@ INSERT INTO users VALUES
 	;
 
 INSERT INTO questions VALUES
-	( 1, 'question 1', 'question body 1'),
-	( 2, 'question 2', 'question body 2'),
-	( 3, 'question 3', 'question body 3')
+	( 1, 1, 'question 1', 'question body 1'), /* question 1 asked by user 1 */
+	( 2, 2, 'question 2', 'question body 2'),
+	( 3, 3, 'question 3', 'question body 3'),
+	( 4, 1, 'question 4', 'question body 4')
 	;
 
 INSERT INTO question_followers VALUES
 	( 1, 1, 2), /* user 1 follows question 2 */
-	( 2, 2, 3)  /* user 2 follows question 3 */
+	( 2, 2, 3), /* user 2 follows question 3 */
+	( 3, 1, 1)  /* user 1 follows quesiton 1 */
 	;
 
 INSERT INTO replies VALUES
 	( 1, NULL, 1, 2, 'user 1 reply to question 2'),
-	( 2, 1,    2, 2, 'user 2 reply to user 1 reply to question 2')
+	( 2, 1,    2, 2, 'user 2 reply to user 1 reply to question 2'),
+	( 3, NULL, 3, 1, 'user 3 reply to question 1')
 	;
 
 INSERT INTO question_likes VALUES
-	( 1, 2)  /* user 1 likes question 2 */
+	( 1, 1, 2), /* user 1 likes question 2 */
+	( 2, 1, 1)  /* user 1 likes quesiton 1 */
 	;
 
