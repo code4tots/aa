6a6505b8118c1ae149b2e14ee3a313b4790fcbc5
math4tots <math4tots@users.noreply.github.com>
2014-08-24 12:20:16 -0700

    rails/goal-setter (not finished)

---
+++ rails/goal-setter/app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  
+  after_initialize do |user|
+    user.session_token ||= self.class.generate_session_token
+  end
+  
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def is_password?(password)
+    # self.password_digest == BCrypt::Password.create(password)
+    ## won't work because BCrypt generates salted digests.
+    ## BCrypt::Password.new ... takes care of this.
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+  
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+  end
+  
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+end
